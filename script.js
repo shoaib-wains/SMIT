f6d6ea2593b86077b5aa9705de21688f2ba14ff4
shoaib-wains <asadvains1@gmail.com>
2023-02-20 04:05:13 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+myVar=5;
+console.log(myVar);
+// Qno 2
+myString="Hello Word"
+alert(myString);
+// Qno 3
+num1=5;
+num2=10;
+userNum=parseInt(prompt("Enter a Number"));
+sum=num1+num2+userNum;
+console.log(sum);
+// Qno 4
+if(myBoolean=true){
+    console.log("It is true");
+}
+else{console.log("It is not true");}
+// Qno 5
+number=parseInt(prompt("Enter a number"));
+if(number/2==1){
+    console.log("It is even");
+}
+else{console.log("It is odd");}
+// Qno 6
+str1="Shoaib ";
+str2="Jutt";
+console.log(str1+str2);
+// Qno 7
+number1=2;
+number2=5;
+product=number1*number2;
+console.log(product);
+// Qno 8
+pass=prompt("Enter Password");
+if (pass=="password"){
+    alert("Access Granted")
+}
+else{alert("Access Denied")}
+// Qno 9
+nmbr1=parseInt(prompt("Enter 1st Number"));
+nmbr2=parseInt(prompt("Enter 2nd Number"));
+nmbr3=parseInt(prompt("Enter 3rd Number"));
+if((nmbr1>nmbr2)&&(nmbr2>nmbr3)){
+    alert(nmbr1+" is Greatest ")
+}
+else if((nmbr2>nmbr3)&&(nmbr3>nmbr1)){
+    alert(nmbr2+" is Greatest ")
+}
+else{alert(nmbr3+" is Greatest ")}
+// Qno 10
+strng1=prompt("Enter Value");
+srtng2=prompt("Enter Value");
+if(strng1==srtng2){
+    alert("Both Strings are Equal");
+}
+else{alert("These String are not Equal");}
